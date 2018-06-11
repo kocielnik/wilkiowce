a0b3359ed2e60f8d6d3358d90ead96d3dea9e3b7
mateusz <m.marzec@firma.bankier.pl>
2018-06-11 11:37:24 +0200

    Fixes Bot.

--- src/Bot.hs
+++ src/Bot.hs
@@ -6,22 +6,20 @@ import Data.Coerce
 import GameElements
 import GameState
 
-data Points = Points Int Int deriving (Eq, Show)
-
 data Position = Position Point deriving (Show, Eq)
 --newtype Position a b = Position (a,b)
 
-surroundingFields :: Points -> [Points]
-surroundingFields (Points x y)
-  | x == 0 && y == 0  = [Points (x+1) (y+1)]
-  | x == 0 && y == 7  = [Points (x+1) (y-1)]
-  | x == 7 && y == 0  = [Points (x-1) (y+1)]
-  | x == 7 && y == 7  = [Points (x-1) (y-1)]
-  | x == 0      = [(Points (x+1) (y-1)), (Points (x+1) (y+1))]
-  | x == 7      = [(Points (x-1) (y-1)), (Points (x-1) (y+1))]
-  | y == 0      = [(Points (x-1) (y+1)), (Points (x+1) (y+1))]
-  | y == 7      = [(Points (x-1) (y-1)), (Points (x+1) (y-1))]
-  | otherwise     = [(Points (x-1) (y-1)), (Points (x-1) (y+1)), (Points (x+1) (y-1)), (Points (x+1) (y+1))]
+surroundingFields :: Point -> [Point]
+surroundingFields (x,y)
+  | x == 0 && y == 0  = [((x+1), (y+1))]
+  | x == 0 && y == 7  = [((x+1), (y-1))]
+  | x == 7 && y == 0  = [((x-1), (y+1))]
+  | x == 7 && y == 7  = [((x-1), (y-1))]
+  | x == 0      = [((x+1), (y-1)), ((x+1), (y+1))]
+  | x == 7      = [((x-1), (y-1)), ((x-1), (y+1))]
+  | y == 0      = [((x-1), (y+1)), ((x+1), (y+1))]
+  | y == 7      = [((x-1), (y-1)), ((x+1), (y-1))]
+  | otherwise     = [((x-1), (y-1)), ((x-1), (y+1)), ((x+1), (y-1)), ((x+1), (y+1))]
 
 isOccupied :: GameState -> Point -> Bool
 isOccupied (GameState w@(Wolf point) [] _) p = point == p
